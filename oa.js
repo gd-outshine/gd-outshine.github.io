0f485b66fb5cb8686de6077a42ccf5f4d94d943b
gd-outshine <glen.dufour@outshine.com>
2025-01-13 17:25:46 -0400

    Update oa.js

--- oa.js
+++ oa.js
@@ -1,11 +1,10 @@
 <script>
 function sendData(data, endpoint) {
   var xhr = new XMLHttpRequest();
-  var stringifiedData = JSON.stringify(data);
 
   xhr.open('POST', endpoint);
   xhr.setRequestHeader('Content-type', 'application/json');
-  xhr.send(stringifiedData);
+  xhr.send();
 
   xhr.onload = function () {
     if (xhr.status.toString()[0] !== '2') {
