db02320a7ac4fcad3ca39cc5b0022938d91890cf
gd-outshine <glen.dufour@outshine.com>
2025-01-13 17:36:44 -0400

    Create oa4.js

---
+++ oa4.js
@@ -0,0 +1,13 @@
+function sendData(endpoint) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('POST', endpoint);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.send();
+
+  xhr.onload = function () {
+    if (xhr.status.toString()[0] !== '2') {
+      console.error(xhr.status + '> ' + xhr.statusText);
+    }
+  };
+}
