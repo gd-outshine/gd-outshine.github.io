de417c9f7209b7c1714495747988fe69e03714a3
gd-outshine <glen.dufour@outshine.com>
2025-01-13 16:45:00 -0400

    Add files via upload

---
+++ oa.js
@@ -0,0 +1,16 @@
+<script>
+function sendData(data, endpoint) {
+  var xhr = new XMLHttpRequest();
+  var stringifiedData = JSON.stringify(data);
+
+  xhr.open('POST', endpoint);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.send(stringifiedData);
+
+  xhr.onload = function () {
+    if (xhr.status.toString()[0] !== '2') {
+      console.error(xhr.status + '> ' + xhr.statusText);
+    }
+  };
+}
+</script>
